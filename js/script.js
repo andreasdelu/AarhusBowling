e2baa7a14c662bf1e9a01fd09847feebfbf406da
andreasdelu <Andreasdelu@gmail.com>
2021-11-16 14:35:20 +0100

    ghkfgh

--- js/script.js
+++ js/script.js
@@ -3,21 +3,22 @@ const menu = document.getElementById("menu");
 const arrow1 = document.getElementById("arrow1");
 const arrow2 = document.getElementById("arrow2");
 const arrow3 = document.getElementById("arrow3");
+const arrow4 = document.getElementById("arrow4");
 const dropdown1 = document.getElementById("drop1");
 const dropdown2 = document.getElementById("drop2");
 const dropdown3 = document.getElementById("drop3");
+const dropdown4 = document.getElementById("drop4");
 const line1 = document.getElementById("topline");
 const line2 = document.getElementById("midline");
 const line3 = document.getElementById("botline");
 
-burger.addEventListener("click", showMenu);
-
-arrow1.addEventListener("click", function() {showDrop(1)});
-arrow2.addEventListener("click", function() {showDrop(2)});
-arrow3.addEventListener("click", function() {showDrop(3)});
+burger.addEventListener("click", function() {showMenu()});
+arrow1.addEventListener("click", function() {showDrop(dropdown1, arrow1)});
+arrow2.addEventListener("click", function() {showDrop(dropdown2, arrow2)});
+arrow3.addEventListener("click", function() {showDrop(dropdown3, arrow3)});
+arrow4.addEventListener("click", function() {showDrop(dropdown4, arrow4)});
 
 var active = false;
-var dropnum = 0;
 
 function showMenu() {
     if (!active) {
@@ -33,12 +34,7 @@ function showMenu() {
     else{
         menu.style.display = "none";
         active = false;
-        arrow1.classList.remove("arrowactive");
-        dropdown1.classList.remove("dropdownactive");
-        arrow2.classList.remove("arrowactive");
-        dropdown2.classList.remove("dropdownactive");
-        arrow3.classList.remove("arrowactive");
-        dropdown3.classList.remove("dropdownactive");
+        resetMenu();
         line1.classList.add("topoff");
         line2.classList.add("midoff");
         line3.classList.add("botoff");
@@ -48,47 +44,24 @@ function showMenu() {
     }
 }
 
-function showDrop(dropnum) {
-    if (dropnum === 1) {
-        if (!arrow1.classList.contains("arrowactive")) {
-        arrow1.classList.add("arrowactive");
-        dropdown1.classList.add("dropdownactive");
-        arrow2.classList.remove("arrowactive");
-        dropdown2.classList.remove("dropdownactive");
-        arrow3.classList.remove("arrowactive");
-        dropdown3.classList.remove("dropdownactive");
-        }
-        else{
-        arrow1.classList.remove("arrowactive");
-        dropdown1.classList.remove("dropdownactive");
-        }
+function resetMenu(){
+    arrow1.classList.remove("arrowactive");
+    dropdown1.classList.remove("dropdownactive");
+    arrow2.classList.remove("arrowactive");
+    dropdown2.classList.remove("dropdownactive");
+    arrow3.classList.remove("arrowactive");
+    dropdown3.classList.remove("dropdownactive");
+    arrow4.classList.remove("arrowactive");
+    dropdown4.classList.remove("dropdownactive");
+}
+
+function showDrop(dropnum, arrownum) {
+    if (!arrownum.classList.contains("arrowactive")) {
+        resetMenu();
+        arrownum.classList.add("arrowactive");
+        dropnum.classList.add("dropdownactive");
     }
-    if (dropnum === 2) {
-        if (!arrow2.classList.contains("arrowactive")) {
-        arrow2.classList.add("arrowactive");
-        dropdown2.classList.add("dropdownactive");
-        arrow1.classList.remove("arrowactive");
-        dropdown1.classList.remove("dropdownactive");
-        arrow3.classList.remove("arrowactive");
-        dropdown3.classList.remove("dropdownactive");
-        }
-        else{
-        arrow2.classList.remove("arrowactive");
-        dropdown2.classList.remove("dropdownactive");
-        }
-    }
-    if (dropnum === 3) {
-        if (!arrow3.classList.contains("arrowactive")) {
-        arrow3.classList.add("arrowactive");
-        dropdown3.classList.add("dropdownactive");
-        arrow2.classList.remove("arrowactive");
-        dropdown2.classList.remove("dropdownactive");
-        arrow1.classList.remove("arrowactive");
-        dropdown1.classList.remove("dropdownactive");
-        }
-        else{
-        arrow3.classList.remove("arrowactive");
-        dropdown3.classList.remove("dropdownactive");
-        }
+    else{
+        resetMenu();
     }
 }
